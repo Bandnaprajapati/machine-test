9be0ef51ea74f1e9ad81c7e2611cbac7b894ae51
bandna prajapati <bandnaprajapati2345@gmail.com>
2022-01-05 19:24:09 +0530

    setup

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use DB;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $latitude  = $request->latitude;
+        $longitude = $request->longitude;
+
+        $users  = User::select("*", DB::raw("6371 * acos(cos(radians(" . $latitude . "))
+                                * cos(radians(latitude)) * cos(radians(longitude) - radians(" . $longitude . "))
+                                + sin(radians(" .$latitude. ")) * sin(radians(latitude))) AS distance"));
+        $users = $users->having('distance', '<', 20);
+        $users = $users->orderBy('distance', 'asc');
+        $users = $users->get();
+         return response()->json([
+            "status" => true,
+            "message"=>"Near By user",
+            "data"=>$users
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         $array = [
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password,
+            'address'=>$request->address,
+            'latitude'=>$request->latitude,
+            'longitude'=>$request->longitude,
+        ];
+
+        user::insert($array);
+        return response()->json([
+            "status" => true,
+            "message"=>"added Successfully",
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
